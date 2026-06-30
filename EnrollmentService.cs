fdc89bab6e1a02e64a7e32252e871674cda67958
Saba Haddis <sabahaddissolomon@gmail.com>
2026-07-01 00:38:34 +0300

    feat: add the guard clauses

--- EnrollmentService.cs
+++ EnrollmentService.cs
@@ -3,6 +3,15 @@ public class EnrollmentService
     public EnrollmentRecord ProcessRegistration(Student? student, Course? course)
     {
         // TODO
+        if (student is null)
+            throw new ArgumentNullException(nameof(student));
+
+        if (course is null)
+            throw new ArgumentNullException(nameof(course));
+
+        if (course.EnrolledCount >= course.Capacity)
+            throw new InvalidOperationException("Course is already full.");
+        
         throw new NotImplementedException();
     }
 }
