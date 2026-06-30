986f72476ff80fa4f1974ac6060e5634adf6fa5a
Saba Haddis <sabahaddissolomon@gmail.com>
2026-06-30 23:04:34 +0300

    feat: null bug in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//warning error: Converting null literal or possible null value to non-nullable type.\
+//if proceeds app will crash at runtime
+string region = null;
+Console.WriteLine(region.ToUpper());
+
--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4756872f1f28a2434dc887b4fecab2582444b69e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
